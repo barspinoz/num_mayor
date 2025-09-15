ece581ecedc80214c63af8bb10a1145026589cea
barspinoz <109839725+barspinoz@users.noreply.github.com>
2025-09-15 11:31:46 -0600

    main 2.0
    se agregó la comprobación de los números y se optimizaron algunos segmentos

--- mayor.c
+++ mayor.c
@@ -7,46 +7,32 @@ int main(){
 	
 	/*variables de salida*/
 	float m=0;
-	float resultado=0;
 	
 	/*dar valores*/
-	printf("\n Ingrese el primer valor: \n \t");
-	scanf(" %f",&a);
-	printf("\n Ingrese el segundo valor: \n \t");
-	scanf(" %f",&b);
-	printf("\n Ingrese el tercer valor: \n \t");
-	scanf(" %f",&c);
+	printf("\n Ingrese tres valores: \n");
+	scanf(" %f %f %f", &a, &b, &c);
 	
-	/*buscar el mayor para a*/
-	if(a<b) {
-	m=b;
+	/*el mayor es b*/
+	if(a < b && c < b) {
+		m=b;
 	}//if
-	else {
-	m=a;
-	}//else
 	
-	/*buscar el mayor para b*/
-	if(b<c) {
-	resultado=c;
+	/*el mayor para a*/
+	if(b < a && c < a) {
+		m=a;
 	}//if
-	if(b>c) {
-			resultado=b;
-	}
 	
-	/*buscar el mayor para c*/
-	if(c<a) {
-	m=a;
-	}//if
-	else {
+	/*el mayor es c*/
+	if(a < c && b < c) {
 		m=c;
-	}//else
+	}
 	
 	/*resultado*/
-	if(resultado>m) {
-		printf("\n El valor mayor es: %f \n",resultado);
+	if(a != b && a != c && b != c) {
+		printf("\n El mayor es: %f \n",m);
 	}
 	else {
-		printf("\n El valor mayor es: %f \n",m);
+		printf("\n ERROR. Vuelva a ingresar los valores. \n");
 	}
 	return 0;
 }//main
